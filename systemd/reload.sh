ff6fc23cabf2e4d52611a377208c5d861c450a0a
Tom Sheppard <tsheppard@solutechnology.com>
2025-04-05 16:33:55 -0400

    script for reloading service files

---
+++ systemd/reload.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# This script is used to reload the systemd services
+# It will stop the current services and start them again with the new configuration
+# Usage: ./reload.sh
+
+# Get the service list from the files in the current directory
+service_list=$(ls ./*.service)
+# Loop through each service file
+for service in $service_list; do
+    # Copy the service file to the systemd directory
+    cp "$service" /etc/systemd/system/
+    # Stop the service
+    systemctl stop "$service"
+    # Reload the systemd manager configuration
+    systemctl daemon-reload
+    # Start the service again
+    systemctl start "$service"
+done
