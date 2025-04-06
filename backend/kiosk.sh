18bd225d7e557a6fc616cb67b0f7d3331bc15189
Tom Sheppard <tjs8938@gmail.com>
2025-04-06 13:17:41 -0400

    Final (?) updates for getting this all running on the pi

--- backend/kiosk.sh
+++ backend/kiosk.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+DISPLAY=":0"
+export DISPLAY=":0"
+USER=uccdga
+
 xset s noblank
 xset s off
 xset -dpms
@@ -9,4 +13,4 @@ sed -i 's/"exited_cleanly":false/"exited_cleanly":true/' /home/"$USER"/.config/c
 sed -i 's/"exit_type":"Crashed"/"exit_type":"Normal"/' /home/"$USER"/.config/chromium/Default/Preferences
 
 pkill chromium
-/usr/bin/chromium-browser --noerrdialogs --disable-infobars --kiosk "$1" &
+/usr/bin/chromium-browser --no-sandbox --noerrdialogs --disable-infobars --kiosk "$1" &
